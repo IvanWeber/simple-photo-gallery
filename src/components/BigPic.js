2cb29c565bd319e3461d0ca2e47cf837da7cd6a9
IvanWeber <ivanbdn@yandex.ru>
2021-02-14 16:02:04 +0300

    title now under pic

--- src/components/BigPic.js
+++ src/components/BigPic.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const BigPic = ({photo}) => {
 
       return <div className="modal-body">
-                  <h1>{photo[0].title}</h1>
                   <img src={photo[0].url} alt="big picture"/>
+                  <h1>{photo[0].title}</h1>
              </div>
 }
